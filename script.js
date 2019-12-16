590e353d422e78a4398487b1b87ce264d8751a04
Pablo Ariño Muñoz <55687027+Pabarino@users.noreply.github.com>
2019-12-16 11:23:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*SCRIPT DE EXAMEN REALIDAD VIRTUAL*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
